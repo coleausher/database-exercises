247ce5ab4ed0c0961a627de5386debe0260c8ab7
Cole Usher <coleausher97@gmail.com>
2023-05-10 15:33:36 -0500

    order by exercises complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+--     Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name ASC, first_name ASC;
+
+
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%e%' ORDER BY emp_no DESC;
+
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
