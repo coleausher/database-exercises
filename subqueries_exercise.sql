a375174a64e920151a0cbc5ee3caf32977110cad
Cole Usher <coleausher97@gmail.com>
2023-05-16 08:32:30 -0500

    subqueries exercise complete

---
+++ subqueries_exercise.sql
@@ -0,0 +1,10 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees
+WHERE hire_date IN (SELECT hire_date FROM employees WHERE emp_no = 101010);
+
+SELECT DISTINCT title from titles
+WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod' );
+
+SELECT first_name, last_name FROM employees
+WHERE gender = 'F' AND emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date = '9999-01-01');
